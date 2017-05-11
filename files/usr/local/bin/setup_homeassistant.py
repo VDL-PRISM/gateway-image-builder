d0ea52be06cb9204178545f7873fcb075cb40c44
Philip Lundrigan <philiplundrigan@gmail.com>
2017-05-11 15:52:27 -0600

    Set permissions of HA configuration file

--- files/usr/local/bin/setup_homeassistant.py
+++ files/usr/local/bin/setup_homeassistant.py
@@ -1,7 +1,11 @@
+import shutil
 import yaml
 
+DEVICE_INIT = '/boot/device-init.yaml'
+HA_CONFIG_FILE = '/home/homeassistant/.homeassistant/configuration.yaml'
+
 # Read in configuration
-with open('/boot/device-init.yaml') as f:
+with open(DEVICE_INIT) as f:
     config = yaml.load(f)
 
 ha_config = config.get('home_assistant_configuration')
@@ -11,5 +15,9 @@ if ha_config is None:
     exit()
 
 # Write Home Assistant configuration
-with open('/home/homeassistant/.homeassistant/configuration.yaml', 'w') as f:
+with open(HA_CONFIG_FILE, 'w') as f:
     f.write(yaml.dump(ha_config, default_flow_style=False))
+
+shutil.chown(HA_CONFIG_FILE,
+             user='homeassistant',
+             group='homeassistant')
