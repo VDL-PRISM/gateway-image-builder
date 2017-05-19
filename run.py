66b3109a75ece53ecc6a3647d60d7bad302646de
Philip Lundrigan <philiplundrigan@gmail.com>
2017-05-19 12:56:18 -0600

    Add option to enter the container

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import subprocess
 
 
 parser = argparse.ArgumentParser(description='Run script to build gateway image')
-parser.add_argument('command', choices=['shell', 'build'])
+parser.add_argument('command', choices=['shell', 'build', 'container'])
 parser.add_argument('orignal_image', help='Image file you want to modify')
 parser.add_argument('new_image', help='Image file that will be created')
 
@@ -31,3 +31,8 @@ elif args.command == 'shell':
                    '-w /usr/rpi gateway-image-builder ' \
                    '/bin/bash -c \'./run.sh images/{}\''.format(path, name), shell=True)
 
+elif args.command == 'container':
+    print("Entering container")
+    subprocess.run('docker run -it --rm --privileged=true -v {}:/usr/rpi/images ' \
+                   '-w /usr/rpi gateway-image-builder ' \
+                   '/bin/bash'.format(path), shell=True)
